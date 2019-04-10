05d5ff6b466d9a55b30f827aa626d01a59da3e29
Admin <Admin@LAPTOP-ICSI5IEF>
2019-04-11 00:13:37 +0700

    Finish

---
+++ ElectronicStore/Controllers/ElectronicStoreController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ElectronicStore.Models;
+namespace ElectronicStore.Controllers
+{
+    public class ElectronicStoreController : Controller
+    {
+        dbElectronicDataContext data = new dbElectronicDataContext();
+        private List<SP> LaySP(int count)
+        {
+            return data.SPs.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
+        }
+        // GET: ElectronicStore
+        public ActionResult Index()
+        {
+            var spmoi = LaySP(7);
+            return View(spmoi);
+        }
+        public ActionResult ThuongHieu()
+        {
+            var thuonghieu = from th in data.THUONGHIEUs select th;
+            return PartialView(thuonghieu);
+        }
+        public ActionResult SpTheoThuongHieu(int id)
+        {
+            var sanpham = from sp in data.SPs where sp.MATH == id select sp ;
+            return View(sanpham);
+        }
+        public ActionResult Details (int id)
+        {
+            var sanpham = from sp in data.SPs where sp.MaSP == id select sp;
+            return View(sanpham.Single());
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ ElectronicStore/Controllers/GioHangController.cs
@@ -0,0 +1,156 @@
+﻿using ElectronicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElectronicStore.Controllers
+{
+    public class GioHangController : Controller
+    {
+        dbElectronicDataContext data = new dbElectronicDataContext();
+        // GET: GioHang
+        public List<GioHang> LayGioHang()
+        {
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang == null)
+            {
+                lstGioHang = new List<GioHang>();
+                Session["GioHang"] = lstGioHang;
+            }
+            return lstGioHang;
+        }
+        public ActionResult ThemGioHang(int iMaSP, string strURL)
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sanpham = lstGioHang.Find(n => n.iMaSP == iMaSP);
+            if (sanpham == null)
+            {
+                sanpham = new GioHang(iMaSP);
+                lstGioHang.Add(sanpham);
+                return Redirect(strURL);
+            }
+            else
+            {
+                sanpham.iSoLuong++;
+                return Redirect(strURL);
+            }
+        }
+        private int TongSoLuong()
+        {
+            int iTongSoLuong = 0;
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang != null)
+            {
+                iTongSoLuong = lstGioHang.Sum(n => n.iSoLuong);
+            }
+            return iTongSoLuong;
+        }
+        private double TongTien()
+        {
+            double iTongTien = 0;
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang != null)
+            {
+                iTongTien = lstGioHang.Sum(n => n.dThanhTien);
+            }
+            return iTongTien;
+        }
+        public ActionResult GioHang()
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            if (lstGioHang.Count == 0)
+            {
+                return RedirectToAction("Index", "ElectronicStore");
+            }
+            ViewBag.TongSoLuong = TongSoLuong();
+            ViewBag.TongTien = TongTien();
+            return View(lstGioHang);
+        }
+        public ActionResult GioHangPartial()
+        {
+            ViewBag.TongSoLuong = TongSoLuong();
+            ViewBag.TongTien = TongTien();
+            return PartialView();
+        }
+        public ActionResult CapnhatGiohang(int iMaSanPham, FormCollection f)
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sanpham = lstGioHang.SingleOrDefault(n => n.iMaSP == iMaSanPham);
+            if (sanpham != null)
+            {
+                sanpham.iSoLuong = int.Parse(f["txtSoLuong"].ToString());
+            }
+            return RedirectToAction("GioHang");
+        }
+        public ActionResult XoaGioHang(int iMaSanPham)
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sanpham = lstGioHang.SingleOrDefault(n => n.iMaSP == iMaSanPham);
+            if (sanpham != null)
+            {
+                lstGioHang.RemoveAll(n => n.iMaSP == iMaSanPham);
+                return RedirectToAction("GioHang");
+
+            }
+            if (lstGioHang.Count == 0)
+            {
+                return RedirectToAction("Index", "ElectronicStore");
+            }
+            return RedirectToAction("GioHang");
+        }
+        public ActionResult XoaTatCaGioHang()
+        {
+            List<GioHang> lstGiohang = LayGioHang();
+            lstGiohang.Clear();
+            return RedirectToAction("Index", "ElectronicStore");
+        }
+        public ActionResult DatHang()
+        {
+            if(Session["Taikhoan"]==null || Session["Taikhoan"].ToString()=="")
+            {
+                return RedirectToAction("Dangnhap", "User");
+            }
+            if(Session["GioHang"]==null)
+            {
+                return RedirectToAction("Index", "ElectronicStore");
+            }
+            List<GioHang> lstGioHang = LayGioHang();
+            ViewBag.TongSoLuong = TongSoLuong();
+            ViewBag.TongTien = TongTien();
+            return View(lstGioHang);
+        }
+        [HttpPost]
+        public ActionResult DatHang (FormCollection collection)
+        {
+            DONDATHANG ddh = new DONDATHANG();
+            KHACHHANG kh = (KHACHHANG)Session["Taikhoan"];
+            List<GioHang> gh = LayGioHang();
+            ddh.MAKH = kh.MaKH;
+            ddh.NgayDat = DateTime.Now;
+            DateTime ngaygiao = DateTime.Parse(collection["Ngaygiao"].ToString());
+            ddh.NgayGiao = ngaygiao;
+            ddh.TinhTrangGiaoHang = false;
+            ddh.DaThanhToan = false;
+            data.DONDATHANGs.InsertOnSubmit(ddh);
+            data.SubmitChanges();
+            foreach(var item in gh)
+            {
+                CTHD cthd = new CTHD();
+                cthd.MaDH = ddh.MADH;
+                cthd.MaSP = item.iMaSP;
+                cthd.SoLuong = item.iSoLuong;
+                cthd.DonGia = (decimal)item.dDonGia;
+                data.CTHDs.InsertOnSubmit(cthd);
+            }
+            data.SubmitChanges();
+            Session["GioHang"] = null;
+            return RedirectToAction("XacNhanDonHang", "GioHang");
+        }
+        public ActionResult XacNhanDonHang()
+        {
+            return View();
+        }
+    }
+}
